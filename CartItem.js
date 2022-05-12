e1ab45463edc0702c64a29f49792f70d42f1e732
Leonardo Moleiro <35323489+leomoleiro95@users.noreply.github.com>
2022-05-12 16:29:50 -0500

    Add files via upload

---
+++ CartItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import "./Cart.css";
+import { cartActions } from "../store/cart-slice";
+
+const CartItem = ({ name, quantity, total, price, id }) => {
+  const dispatch = useDispatch();
+
+  const decrementCartItem = () => {
+    dispatch(cartActions.removeFromCart(id));
+  };
+  const incrementCartItems = () => {
+    dispatch(
+      cartActions.addToCart({
+        name,
+        id,
+        price,
+      })
+    );
+  };
+  return (
+    <div className="cartItem">
+      <h2> {name}</h2>
+      <p>${price} /-</p>
+      <p>x{quantity}</p>
+      <article>Total ${total}</article>
+      <button className="cart-actions" onClick={decrementCartItem}>
+        -
+      </button>
+      <button className="cart-actions" onClick={incrementCartItems}>
+        +
+      </button>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ cart-actions.js
@@ -0,0 +1,61 @@
+import { cartActions } from "./cart-slice";
+import { uiActions } from "./ui-slice";
+
+export const fetchData = () => {
+    return async (dispatch) => {
+        const fetchHandler = async () => {
+            const res = await fetch('https://shopping-http-default-rtdb.firebaseio.com/cartItems.json');
+
+            const data = await res.json();
+            return data;
+        }
+        try {
+            const cartData = await fetchHandler();
+            dispatch(cartActions.replaceData(cartData))
+        } catch (err) {
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: "Request to database: Failed",
+                type: 'error'
+            }))
+        };
+    }
+}
+
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            open: true,
+            message: "Sending Request",
+            type: 'warning'
+        })
+        );
+        const sendRequest = async () => {
+            // Send State as Sending request
+
+            const res = await fetch('https://shopping-http-default-rtdb.firebaseio.com/cartItems.json', {
+                method: "PUT",
+                body: JSON.stringify(cart)
+            }
+            );
+            const data = await res.json();
+            //Send state as request is successfull
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: "Request to database: Success",
+                type: 'success'
+            })
+            );
+        };
+        try {
+            await sendRequest()
+        } catch (err) {
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: "Request to database: Failed",
+                type: 'error'
+            }))
+        };
+    }
+}
